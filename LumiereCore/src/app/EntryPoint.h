20f248a574d04225743721270975b86ec588745f
Matthias <rocketpencil.studios@gmail.com>
2025-04-14 15:55:50 +0200

    Refactor EntryPoint.h

--- LumiereCore/src/app/EntryPoint.h
+++ LumiereCore/src/app/EntryPoint.h
@@ -2,18 +2,10 @@
 
 extern Lumiere::Application* Lumiere::CreateApplication(int argc, char** argv);
 
-namespace Lumiere {
-
-int Main(int argc, char** argv) {
-	Lumiere::Application* app = CreateApplication(argc, argv);
+int main(int argc, char** argv) {
+	Lumiere::Application* app = Lumiere::CreateApplication(argc, argv);
 	app->Run();
 	delete app;
 
 	return 0;
-}
-
-}
-
-int main(int argc, char** argv) {
-	return Lumiere::Main(argc, argv);
 }
